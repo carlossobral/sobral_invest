4daac8daa5f74d6ec1cdaa02e452712679ea0970
carlossobral <carlossobral87@gmail.com>
2026-05-10 01:02:10 -0300

    Update brapi_client.py

--- brapi_client.py
+++ brapi_client.py
@@ -1,20 +1,21 @@
 import requests
+from config import BRAPI_TOKEN, BASE_URL
 
 def buscar_acoes(lista_tickers):
+    tickers = ",".join(lista_tickers)
+    url = f"{BASE_URL}/quote/{tickers}"
 
-    token = "SEU_TOKEN_AQUI"
+    params = {"token": BRAPI_TOKEN}
 
-    url = "https://brapi.dev/api/quote/PETR4"
+    response = requests.get(url, params=params)
 
-    response = requests.get(
-        url,
-        params={
-            "token": token
-        }
-    )
+    print("STATUS CODE:", response.status_code)
+    print("RESPOSTA API:", response.text[:500])
 
-    print("STATUS:", response.status_code)
-    print("RESPOSTA:")
-    print(response.text)
+    if response.status_code != 200:
+        return []
 
-    return []
+    data = response.json()
+    resultados = data.get("results", [])
+    print("TOTAL RESULTADOS:", len(resultados))
+    return resultados
