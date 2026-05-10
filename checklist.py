5c26cc49ee372eaf8c3f91370ea9a863459aa8f6
carlossobral <carlossobral87@gmail.com>
2026-05-10 12:10:41 -0300

    Update checklist.py

--- checklist.py
+++ checklist.py
@@ -1,16 +1,18 @@
 def checklist_buy_hold(ind):
-
     score = 0
-
     checklist = {}
 
-    checklist["ROE > 12%"] = ind["ROE"] > 0.12
-    checklist["DY > 7%"] = ind["DY"] > 0.07
-    checklist["Dívida baixa"] = ind["Divida_PL"] < 1
-    checklist["Liquidez Corrente"] = ind["Liquidez_Corrente"] > 1
+    roe = ind.get("ROE") or 0
+    dy = ind.get("DY") or 0
+    divida_pl = ind.get("Divida_PL") or 0
+    liquidez = ind.get("Liquidez_Corrente") or 0
+
+    checklist["ROE > 12%"] = roe > 12
+    checklist["DY > 7%"] = dy > 7
+    checklist["Dívida baixa"] = divida_pl < 1
+    checklist["Liquidez Corrente > 1"] = liquidez > 1
 
     for item in checklist.values():
-
         if item:
             score += 10
 
