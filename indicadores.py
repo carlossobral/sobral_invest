e1e00fcc8cfa14699a6c72fef7cfe32ff37fb3af
carlossobral <carlossobral87@gmail.com>
2026-05-09 22:44:14 -0300

    Create indicadores.py

---
+++ indicadores.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+def calcular_indicadores(data):
+
+    indicadores = {}
+
+    try:
+
+        preco = data.get("regularMarketPrice", 0)
+
+        dy = data.get("dividendYield", 0)
+        pl = data.get("priceEarnings", 0)
+        pvp = data.get("priceToBook", 0)
+
+        roe = data.get("returnOnEquity", 0)
+        roa = data.get("returnOnAssets", 0)
+
+        margem_liquida = data.get("profitMargins", 0)
+        margem_ebit = data.get("operatingMargins", 0)
+        margem_bruta = data.get("grossMargins", 0)
+
+        divida_pl = data.get("debtToEquity", 0)
+
+        ev_ebitda = data.get("enterpriseToEbitda", 0)
+
+        receita_growth = data.get("revenueGrowth", 0)
+        lucro_growth = data.get("earningsGrowth", 0)
+
+        liquidez_corrente = data.get("currentRatio", 0)
+
+        indicadores = {
+
+            "Cotacao": preco,
+
+            "DY": dy,
+            "PL": pl,
+            "PVP": pvp,
+            "EV_EBITDA": ev_ebitda,
+
+            "ROE": roe,
+            "ROA": roa,
+
+            "Margem_Liquida": margem_liquida,
+            "Margem_EBIT": margem_ebit,
+            "Margem_Bruta": margem_bruta,
+
+            "Divida_PL": divida_pl,
+
+            "Receita_CAGR": receita_growth,
+            "Lucro_CAGR": lucro_growth,
+
+            "Liquidez_Corrente": liquidez_corrente
+        }
+
+    except Exception as e:
+
+        print("Erro indicadores:", e)
+
+    return indicadores
